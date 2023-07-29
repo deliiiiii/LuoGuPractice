a54d5637f2f09360a51071f01784ca4f65fccbc5
DeliGitee <1765939870@qq.com>
2023-07-29 10:24:58 -0400

    P1449 后缀表达式

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -1,22 +1,102 @@
-//#include <iostream>
-//using namespace std;
-
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+using namespace std;
 
-long long n, f[20][20];
-long long dfs(int x, int y)
+template <class T, int size>
+class Array
 {
-    if (x == 0) return 1;
-    if (f[x][y] != 0) return f[x][y];
-    if (y > 0) f[x][y] += dfs(x, y - 1);
-    f[x][y] += dfs(x - 1, y + 1);
-    return f[x][y];
-}
+public:
+    Array()
+    {
+        m_count = 0; 
+        m_size = size;
+    }
+    void Push(T elem) 
+    {
+        array[m_count] = elem;
+        m_count++;
+    }
+    T Pop()
+    {
+        if (m_count == 0)return -1;
+        return array[(m_count--) - 1];
+    }
+    void display()
+    {
+        int i;
+        for (i = 0; i < m_count; i++)
+            cout << array[i] << " ";
+        cout << endl;
+    }
+    public:
+        T array[size];
+        int m_count;
+        int m_size;
+};
+
+//31.5.21.-*7.+@
+//31.5.21.++@
 int main()
 {
-    scanf("%lld", &n);
-    printf("%lld", dfs(n, 0));
+    Array<int, 100>  class_array_char;
+    bool isContinue = false;
+    for (int i = 0; i < class_array_char.m_size; i++)
+    {
+        char t;
+        scanf("%c", &t);
+        if (t == '.')
+        {
+            isContinue = false;
+            continue;
+        }
+        if (t == '@')
+        {
+            isContinue = false;
+            break;
+        }
+        
+        if (t >= '0' && t <= '9')
+        {
+            if (isContinue)
+            {
+                class_array_char.Push(class_array_char.Pop()*10 + t - '0');
+            }
+            else
+            {
+                class_array_char.Push(t - '0');
+            }
+            isContinue = true;
+        }
+        else
+        {
+            isContinue = false;
+            if (t == '+')
+            {
+                int int_2 = class_array_char.Pop();
+                int int_1 = class_array_char.Pop();
+                class_array_char.Push(int_1 + int_2);
+            }if (t == '-')
+            {
+                int int_2 = class_array_char.Pop();
+                int int_1 = class_array_char.Pop();
+                class_array_char.Push(int_1 - int_2);
+            }if (t == '*')
+            {
+                int int_2 = class_array_char.Pop();
+                int int_1 = class_array_char.Pop();
+                class_array_char.Push(int_1 * int_2);
+            }if (t == '/')
+            {
+                int int_2 = class_array_char.Pop();
+                int int_1 = class_array_char.Pop();
+                class_array_char.Push(int_1 / int_2);
+            }
+        }
+    }
+    printf("%d", class_array_char.array[0]);
     return 0;
 }
+
+
+
+

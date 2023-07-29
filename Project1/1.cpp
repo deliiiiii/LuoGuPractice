a1ff45db3d37eda16eed7359b41c2c4ad56bdcdc
DeliGitee <1765939870@qq.com>
2023-07-29 13:49:10 -0400

    P1162 填涂颜色

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -2,66 +2,68 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main()
+int n;
+vector<vector<int>> map;
+int dx[5] = { 0,-1,1,0,0 };
+int dy[5] = { 0,0,0,-1,1 };
+void Color3(int x, int y)
 {
-    vector<int>weight;
-    vector<int>index_max_weight;
-    int n;
-    cin >> n;
-    int inst, x;
+    if (x < 0 || y < 0 || x >= n || y >= n)
+    {
+        return;
+    }
+    if (map[x][y] == 3 || map[x][y] == 1)
+    {
+        return;
+    }
+    if (map[x][y] == 0)
+    {
+        map[x][y] = 3;
+    }
+    for (int i = 1; i <= 4; i++)
+    {
+        Color3(x + dx[i], y + dy[i]);
+    }
+}
+void Remove3_and_Color2()
+{
     for (int i = 0; i < n; i++)
     {
-        cin >> inst;
-        switch (inst)
+        for (int j = 0; j < n; j++)
         {
-            case 0:
+            if (map[i][j] == 0)map[i][j] = 2;
+            if (map[i][j] == 3)map[i][j] = 0;
+            cout << map[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        map.push_back(vector<int>());
+        for (int j = 0; j < n; j++)
+        {
+            int t;
+            cin >> t;
+            map[i].push_back(t);
+        }
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n-1 || j == n-1)
             {
-                cin >> x;
-                
-                
-                weight.push_back(x);
-                if (weight.size() == 1)
-                {
-                    index_max_weight.push_back(0);
-                }
-                else
-                {
-                    int size = index_max_weight.size();
-                    if (x > weight[index_max_weight[index_max_weight.size() - 1]])
-                    {
-                        index_max_weight.push_back(size);
-                    }
-                    else
-                    {
-                        index_max_weight.push_back(index_max_weight[size - 1]);
-                    }
-                }
-                break;
-            }
-            case 1:
-            {   
-                if (index_max_weight.size() == 0)
-                {
-                    continue;
-                }
-                weight.pop_back();
-                index_max_weight.pop_back();
-                break;
-            }
-            case 2:
-            {
-                if (index_max_weight.size() == 0)
-                {
-                    cout << 0 << endl;
-                }
-                else
-                {
-                    cout << weight[index_max_weight[index_max_weight.size() - 1]] << endl;
-                }
-                break;
+                Color3(i, j);
             }
         }
     }
+    Remove3_and_Color2();
     return 0;
 }
 

546d94d180a71cacc3300a89cad0de2d2007c285
DeliGitee <1765939870@qq.com>
2023-07-29 14:25:13 -0400

    P1042 乒乓球

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -2,68 +2,99 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int n;
-vector<vector<int>> map;
-int dx[5] = { 0,-1,1,0,0 };
-int dy[5] = { 0,0,0,-1,1 };
-void Color3(int x, int y)
+int target_score = 11;
+int deltaScore = 2;
+int score_1 = 0,score_2 = 0;
+int roundScore_1 = 0, roundScore_2 = 0;
+vector<char>input;
+int absDelta(int a, int b)
 {
-    if (x < 0 || y < 0 || x >= n || y >= n)
-    {
-        return;
-    }
-    if (map[x][y] == 3 || map[x][y] == 1)
-    {
-        return;
-    }
-    if (map[x][y] == 0)
-    {
-        map[x][y] = 3;
-    }
-    for (int i = 1; i <= 4; i++)
-    {
-        Color3(x + dx[i], y + dy[i]);
-    }
+    return a > b ? a - b : b - a;
 }
-void Remove3_and_Color2()
+bool LowDeltaScore()
 {
-    for (int i = 0; i < n; i++)
+    return absDelta(score_1, score_2) < 2;
+}
+void Execute()
+{
+    char t;
+    scanf("%c", &t);
+    input.push_back(t);
+    while (t != EOF)
     {
-        for (int j = 0; j < n; j++)
+        if (t == '\n')
         {
-            if (map[i][j] == 0)map[i][j] = 2;
-            if (map[i][j] == 3)map[i][j] = 0;
-            cout << map[i][j] << " ";
+            scanf("%c", &t);
+            input.push_back(t);
+            continue;
         }
-        cout << endl;
+        if (t == 'W')score_1++;
+        if (t == 'L')score_2++;
+        if (t == 'E')
+        {
+            cout << score_1 << ":" << score_2 << endl;
+            break;
+        }
+        if (score_1 < target_score && score_2 < target_score
+            ||
+            ((score_1 >= target_score || score_2 >= target_score) && LowDeltaScore())
+            )
+        {
+        }
+        else
+        {
+            cout << score_1 << ":" << score_2 << endl;
+            score_1 > score_2 ? roundScore_1++ : roundScore_2++;
+            score_1 = score_2 = 0;
+        }
+        scanf("%c", &t);
+        input.push_back(t);
     }
 }
-
+void Execute_2()
+{
+    char t;
+    t = input[0];
+    input.erase(input.begin());
+    while (t != EOF)
+    {
+        if (t == '\n')
+        {
+            t = input[0];
+            input.erase(input.begin());
+            continue;
+        }
+        if (t == 'W')score_1++;
+        if (t == 'L')score_2++;
+        if (t == 'E')
+        {
+            cout << score_1 << ":" << score_2 << endl;
+            break;
+        }
+        if (score_1 < target_score && score_2 < target_score
+            ||
+            ((score_1 >= target_score || score_2 >= target_score) && LowDeltaScore())
+            )
+        {
+        }
+        else
+        {
+            cout << score_1 << ":" << score_2 << endl;
+            score_1 > score_2 ? roundScore_1++ : roundScore_2++;
+            score_1 = score_2 = 0;
+        }
+        t = input[0];
+        input.erase(input.begin());
+    }
+}
 int main()
 {
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        map.push_back(vector<int>());
-        for (int j = 0; j < n; j++)
-        {
-            int t;
-            cin >> t;
-            map[i].push_back(t);
-        }
-    }
-    
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == 0 || j == 0 || i == n-1 || j == n-1)
-            {
-                Color3(i, j);
-            }
-        }
-    }
-    Remove3_and_Color2();
+    Execute();
+    cout << endl;
+    score_1 = score_2 = 0;
+    roundScore_1 = roundScore_2 = 0;
+    target_score = 21;
+    Execute_2();
     return 0;
 }
 

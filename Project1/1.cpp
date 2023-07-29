d74724e22bf08e726d7d84813dec35032131322f
DeliGitee <1765939870@qq.com>
2023-07-29 13:19:44 -0400

    P1165 日志分析

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -1,102 +1,67 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <vector>
 using namespace std;
-
-template <class T, int size>
-class Array
-{
-public:
-    Array()
-    {
-        m_count = 0; 
-        m_size = size;
-    }
-    void Push(T elem) 
-    {
-        array[m_count] = elem;
-        m_count++;
-    }
-    T Pop()
-    {
-        if (m_count == 0)return -1;
-        return array[(m_count--) - 1];
-    }
-    void display()
-    {
-        int i;
-        for (i = 0; i < m_count; i++)
-            cout << array[i] << " ";
-        cout << endl;
-    }
-    public:
-        T array[size];
-        int m_count;
-        int m_size;
-}; 
-Array<long long int,5>  class_array_int;
-//6+2*3*4+4
 int main()
 {
-    bool isContinue = false,isMultiply = false;
-    long long int result = 0;
-    int m = 10000;
-    for (int i = 0; ; )
+    vector<int>weight;
+    vector<int>index_max_weight;
+    int n;
+    cin >> n;
+    int inst, x;
+    for (int i = 0; i < n; i++)
     {
-        char t;
-        scanf("%c", &t);
-        if (t == EOF || t == '\n')
+        cin >> inst;
+        switch (inst)
         {
-            if (isMultiply)
+            case 0:
             {
-                long long int int_2 = class_array_int.Pop();
-                long long int int_1 = class_array_int.Pop();
-                class_array_int.Push(int_2 * int_1 % m);
-            }
-            result += class_array_int.Pop();
-            result %= m;
-            break;
-        }
-        if (t >= '0' && t <= '9')
-        {
-            if (isContinue)
-            {
-                class_array_int.Push(class_array_int.Pop()*10 + t - '0');
-            }
-            else
-            {
-                class_array_int.Push(t - '0');
-            }
-            isContinue = true;
-        }
-        else
-        {
-            isContinue = false;
-            if (t == '+')
-            {
-                if (isMultiply)
+                cin >> x;
+                
+                
+                weight.push_back(x);
+                if (weight.size() == 1)
                 {
-                    long long int int_2 = class_array_int.Pop();
-                    long long int int_1 = class_array_int.Pop();
-                    class_array_int.Push(int_2 * int_1 % m);
+                    index_max_weight.push_back(0);
                 }
-                result += class_array_int.Pop();
-                result %= m;
-                isMultiply = false;
-                continue;
-            }
-            if (t == '*')
-            {
-                if (isMultiply)
+                else
                 {
-                    long long int int_2 = class_array_int.Pop();
-                    long long int int_1 = class_array_int.Pop();
-                    class_array_int.Push(int_2 * int_1 % m);
+                    int size = index_max_weight.size();
+                    if (x > weight[index_max_weight[index_max_weight.size() - 1]])
+                    {
+                        index_max_weight.push_back(size);
+                    }
+                    else
+                    {
+                        index_max_weight.push_back(index_max_weight[size - 1]);
+                    }
                 }
-                isMultiply = true;
+                break;
+            }
+            case 1:
+            {   
+                if (index_max_weight.size() == 0)
+                {
+                    continue;
+                }
+                weight.pop_back();
+                index_max_weight.pop_back();
+                break;
+            }
+            case 2:
+            {
+                if (index_max_weight.size() == 0)
+                {
+                    cout << 0 << endl;
+                }
+                else
+                {
+                    cout << weight[index_max_weight[index_max_weight.size() - 1]] << endl;
+                }
+                break;
             }
         }
     }
-    printf("%d", result);
     return 0;
 }
 

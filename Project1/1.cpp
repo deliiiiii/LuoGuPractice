f355d9198f0aa377947395374e0d4c092d5cd556
DeliGitee <1765939870@qq.com>
2023-07-30 08:34:33 -0400

    P3367 【模板】并查集

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -5,18 +5,53 @@
 #include <string.h>
 using namespace std;
 
-string a, b;
-void DFS(string x, string y) {
-	if (!(int)y.size()) return;
-	int pos = x.find(y[0]);
-	DFS(x.substr(0, pos), y.substr(1, pos));
-	DFS(x.substr(pos + 1), y.substr(pos + 1));
-	cout << y[0];
+int n, m;
+int f[10005];
+
+int SetParent(int cur, int parent)
+{
+	if (f[cur] != cur)
+	{
+		f[cur] = SetParent(f[cur], parent);
+	}
+	else
+	{
+		return f[cur] = parent;
+	}
+}
+int FindParent(int cur)
+{
+	if (f[cur] != cur)return FindParent(f[cur]);
+	return cur;
+}
+bool Check(int e1, int e2)
+{
+	return FindParent(e1) == FindParent(e2);
 }
 int main() 
 {
-	cin >> a >> b;
-	DFS(a, b);
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		f[i] = i;
+	}
+	for (int i = 1; i <= m; i++)
+	{
+		int cata, elem_1, elem_2;
+		cin >> cata >> elem_1 >> elem_2;
+		if (cata == 1)
+		{
+			if (!Check(elem_1, elem_2))
+			{
+				SetParent(elem_1, elem_2);
+			}
+		}
+		else if (cata == 2)
+		{
+			if (Check(elem_1, elem_2))cout << "Y" << endl;
+			else cout << "N" << endl;
+		}
+	}
 	return 0;
 }
 

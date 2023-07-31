5510afdb64b53b2240ef5b376c0fb85e71bbe22e
DeliGitee <1765939870@qq.com>
2023-07-31 12:06:53 -0400

    P5788 【模板】单调栈

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -3,75 +3,41 @@
 #include <vector>
 #include <algorithm>
 #include <string.h>
-#include<queue>
+#include<stack>
 using namespace std;
-const int max_team = 1e3 + 10;
-const int max_teamer = 1e3 + 10;
-queue<int>queue_index_team;
-queue<int>queue_index_teamer[max_team];
-int count_team, count_teamer;
-int index_teamer[max_team][max_teamer];
-int myTeamIndex[1000000];
-void Clear()
+const int max_n = 3 * 1e6 + 10;
+struct Node
 {
-    while (!queue_index_team.empty())
-        queue_index_team.pop();
-    for (int i = 0; i < max_team; i++)
-    {
-        while (!queue_index_teamer[i].empty())
-            queue_index_teamer[i].pop();
-    }
-}
+    int index;
+    int value;
+}nums[max_n];
+
+stack<Node>stack_nums;
+int n;
+int ans[max_n];
 int main()
 {
-    int count_test = 1;
-    while (1)
+    cin >> n;
+    for (int i = 1; i <= n; i++)
     {
-        scanf("%d", &count_team);
-        if (count_team == 0)
-            return 0;
-        Clear();
-        cout << "Scenario #" << count_test << endl;
-        for (int i = 1; i <= count_team; i++)
+        nums[i].index = i;
+        cin >> nums[i].value;
+        while (!stack_nums.empty() && stack_nums.top().value < nums[i].value)
         {
-            cin >> count_teamer;
-            for (int j = 1; j <= count_teamer; j++)
-            {
-                cin >> index_teamer[i][j];
-                myTeamIndex[index_teamer[i][j]] = i;
-            }
+            ans[stack_nums.top().index] = i;
+            stack_nums.pop();
         }
-        
-        while (1)
-        {
-            char cata[20];
-            scanf("%s", cata);
-            if (cata[0] == 'S')
-            {
-                count_test++;
-                cout << endl;
-                break;
-            }
-            if (cata[0] == 'E')
-            {
-                int added;
-                scanf("%d", &added);
-                if (queue_index_teamer[myTeamIndex[added]].empty())
-                {
-                    queue_index_team.push(myTeamIndex[added]);
-                }
-                queue_index_teamer[myTeamIndex[added]].push(added);
-            }
-            if (cata[0] == 'D')
-            {
-                cout << queue_index_teamer[queue_index_team.front()].front() << endl;
-                queue_index_teamer[queue_index_team.front()].pop();
-                if (queue_index_teamer[queue_index_team.front()].empty())
-                {
-                    queue_index_team.pop();
-                }
-            }
-        }
+        stack_nums.push(nums[i]);
+    }
+    while (!stack_nums.empty())
+    {
+        ans[stack_nums.top().index] = 0;
+        stack_nums.pop();
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cout << ans[i] << " ";
     }
     return 0;
+
 }

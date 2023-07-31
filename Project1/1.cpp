aaf5ccaf47a81cc89b18298771f904b4ffe02742
DeliGitee <1765939870@qq.com>
2023-07-31 09:48:30 -0400

    CF44C Holidays

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -5,48 +5,35 @@
 #include <string.h>
 
 using namespace std;
-const int max_position = 100000 + 10;
-int n, q,m;
-struct Pos
-{
-    int index;
-    int num;
-}pos[max_position];
-bool Cmp(Pos x, Pos y)
-{
-    return x.num < y.num;
-}
-void HalfDivide()
-{
-    int left = 1, right = n;
-    while (left <= right)
-    {
-        int mid = (left + right) / 2;
-        if (m > pos[mid].num) 
-            left = mid + 1;
-        else 
-            right = mid - 1;
-    }
-    if (pos[left].num == m)
-        cout << pos[left].index << endl;
-    else
-        cout << "0" << endl;
-}
+const int max_n = 100 + 10;
+const int max_m = 100 + 10;
+int n, m;
+int watered[max_n];
 int main()
 {
-    cin >> n;
+    cin >> n >> m;
     for (int i = 1; i <= n; i++)
     {
-        pos[i].index = i;
-        cin >> pos[i].num;
+        watered[i] = 0;
     }
-    sort(pos + 1, pos + 1 + n, Cmp);
-    cin >> q;
-    for (int i = 0; i < q; i++)
+    for(int i=0;i<m;i++)
     {
-        cin >> m;
-        HalfDivide();
+        int day1, day2;
+        cin >> day1 >> day2;
+        for (int j = day1; j <= day2; j++)
+        {
+            watered[j] ++;
+        }
     }
+    for (int i = 1; i <= n; i++)
+    {
+        if (watered[i] != 1)
+        {
+            cout << i << " " << watered[i];
+            return 0;
+        }
+    }
+    cout << "OK";
     return 0;
 
 }

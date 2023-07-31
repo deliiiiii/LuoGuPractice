8abae6360899815e73a1206a71d119b9040157ee
DeliGitee <1765939870@qq.com>
2023-07-31 12:23:45 -0400

    P3719 [AHOI2017初中组] rexp

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -1,43 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string.h>
-#include<stack>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-const int max_n = 3 * 1e6 + 10;
-struct Node
+const int max_n = 1 * 1e5 + 10;
+int work()
 {
-    int index;
-    int value;
-}nums[max_n];
+	int count = 0;
+	char ch;
+	while( cin >> ch)
+	{
+		if (ch == ')')
+			return count;
+		else if (ch == '(')
+			count += work();
+		else if (ch == '|')
+			return max(count, work());
+		else
+			count++;
+	}
+	return count;
+}
 
-stack<Node>stack_nums;
-int n;
-int ans[max_n];
 int main()
 {
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        nums[i].index = i;
-        cin >> nums[i].value;
-        while (!stack_nums.empty() && stack_nums.top().value < nums[i].value)
-        {
-            ans[stack_nums.top().index] = i;
-            stack_nums.pop();
-        }
-        stack_nums.push(nums[i]);
-    }
-    while (!stack_nums.empty())
-    {
-        ans[stack_nums.top().index] = 0;
-        stack_nums.pop();
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        cout << ans[i] << " ";
-    }
-    return 0;
-
+	cout << work();
+	return 0;
 }

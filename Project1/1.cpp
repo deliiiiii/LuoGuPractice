6922392be448b1038d3bc595897912e20fdeaefc
DeliGitee <1765939870@qq.com>
2023-08-01 13:16:06 -0400

    P5318 【深基18.例3】查找文献

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -1,111 +1,66 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<vector>
+#include <queue>
 #include<algorithm>
-
 #include<cstring>
 using namespace std;
-const int max_n = 1 * 1e2 + 10;
+const int max_n = 1 * 1e5 + 10;
 
 
 
-struct TreeNode
+std::vector<int> nearChart[max_n];
+int n, m;
+bool visited[max_n];
+queue<int> q;
+
+void dfs(int x, int cur) 
 {
-    int index;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode* father;
-};
-TreeNode* node[max_n];
-void AddArc(int n1, int n2)
-{
-    if (!node[n1]->left)
-    {
-        node[n1]->left = node[n2];
-        node[n2]->father = node[n1];
-    }
-    else
-    {
-        node[n1]->right = node[n2];
-        node[n2]->father = node[n1];
-    }
-}
-int Btdepth(TreeNode* T) {
-    if (T == NULL)
-        return 0;
-    int ldep = Btdepth(T->left);
-    int rdep = Btdepth(T->right);
-    if (ldep > rdep)
-        return ldep + 1;
-    else
-        return rdep + 1;
-}
-int  my_count[100];
-int max_width = -1;
-
-void FindWidth(TreeNode* T, int k)
-{
-    if (T == NULL)
-        return;
-    my_count[k]++;
-    if (max_width < my_count[k])
-        max_width = my_count[k];
-    FindWidth(T->left, k + 1);
-    FindWidth(T->right, k + 1);
+	visited[x] = true;
+	cout << x << " ";
+	if (cur == n) 
+		return;
+	for (int i = 0; i < nearChart[x].size(); i++)
+		if (!visited[nearChart[x][i]]) 
+			dfs(nearChart[x][i], cur + 1);
 }
 
-//MAX即为所求宽度
-TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+void bfs(int x) 
 {
-    if (root == p || root == q || !root)return root;
-
-    TreeNode* left = lowestCommonAncestor(root->left, p, q);
-    TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
-    if (!left && !right)return NULL;
-    else if (left && !right)return left;
-    else if (right && !left)return right;
-
-    return root;
+	visited[x] = true;
+	q.push(x);
+	while (!q.empty()) 
+	{
+		int temp = q.front();
+		q.pop();
+		cout << temp << " ";
+		for (int i = 0; i < nearChart[temp].size(); i++)
+		{
+			if (!visited[nearChart[temp][i]])
+			{
+				visited[nearChart[temp][i]] = true;
+				q.push(nearChart[temp][i]);
+			}
+		}
+	}
 }
 
-int n;
-int main()
+int main() 
 {
-
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        node[i] = (TreeNode*)malloc(sizeof(TreeNode));
-        node[i]->index = i;
-        node[i]->left = NULL;
-        node[i]->right = NULL;
-        node[i]->father = NULL;
-    }
-    int n1, n2;
-    for (int i = 1; i <= n - 1; i++)
-    {
-        cin >> n1 >> n2;
-        AddArc(n1, n2);
-    }
-    cin >> n1 >> n2;
-    int target = lowestCommonAncestor(node[1], node[n1], node[n2])->index;
-    int ans = 0;
-    TreeNode* t1 = node[n1];
-    TreeNode* t2 = node[n2];
-    while (t1->index != target)
-    {
-        t1 = t1->father;
-        ans += 2;
-    }
-    while (t2->index != target)
-    {
-        t2 = t2->father;
-        ans += 1;
-    }
-    cout << Btdepth(node[1]) << endl;
-    FindWidth(node[1], 1);
-    cout << max_width << endl;
-    cout << ans;
-    return 0;
+	cin >> n >> m;
+	for (int i = 1; i <= m; i++) 
+	{
+		int u, v;
+		cin >> u >> v;
+		nearChart[u].push_back(v);
+	}
+	for (int i = 1; i <= n; i++) 
+		sort(nearChart[i].begin(), nearChart[i].end());
+	memset(visited, false, sizeof(visited));
+	dfs(1, 0);
+	cout << endl;
+	memset(visited, false, sizeof(visited));
+	bfs(1);
+	cout << endl;
+	return 0;
 }

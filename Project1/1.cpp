62bcbd06e102a15c3549bd73c9ad07356eb0b495
DeliGitee <1765939870@qq.com>
2023-08-04 10:46:55 -0400

    P1309 [NOIP2011 普及组] 瑞士轮

--- Project1/1.cpp
+++ Project1/1.cpp
@@ -1,52 +1,95 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<vector>
-#include <queue>
+#include<queue>
 #include<algorithm>
 #include<cstring>
+using std::sort;
 using namespace std; 
 
-const int max_n = 5 * 1e5 + 10;
-struct node
-{
-    int val, num;
-}a[max_n];
-int n;
-long long ans = 0;
-int c[max_n];
-void add(int x, int k)
-{
-    for (int i = x; i <= n; i += i & (-i)) 
-        c[i] += k;
-}
-int query(int x)
-{
-    int sum = 0;
-    for (int i = x; i > 0; i -= i & (-i))
-        sum += c[i];
-    return sum;
-}
-int cmp(node x, node y)
-{ 
-    if (x.val == y.val) 
-        return x.num > y.num; 
-    return x.val > y.val; 
-}
+const int max_n = 1 * 1e5 + 10;
 
+struct Competitor 
+{
+    int n, k, score;
+};
+Competitor init[200050];
+Competitor mid1[100050], c[100050];
+Competitor final[200050];
+int n, R, Q;
+int read()
+{
+    int x = 0;
+    char c = getchar();
+    while (c > '9' || c < '0') 
+        c = getchar();
+    while (c >= '0' && c <= '9')
+    {
+        x = x * 10 + c - '0';
+        c = getchar();
+    }
+    return x;
+}
+bool cmp(Competitor x, Competitor y)
+{
+    if (x.score != y.score) 
+        return x.score > y.score;
+    return x.n < y.n;
+}
+void work()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (init[2 * i - 1].k > init[2 * i].k)
+        {
+            init[2 * i - 1].score++, mid1[i] = init[2 * i - 1], c[i] = init[2 * i];
+        }
+        else
+        {
+            init[2 * i].score++, mid1[i] = init[2 * i], c[i] = init[2 * i - 1];
+        }
+    }
+        
+    int i = 1, j = 1, k = 1;
+    while (i <= n && j <= n)
+    {
+        if (cmp(mid1[i], c[j]))
+        {
+            final[k] = mid1[i];
+            k++, i++;
+        }
+
+        else
+        {
+            final[k] = c[j];
+            k++, j++;
+        }
+    }
+    while (i <= n)
+    {
+        final[k] = mid1[i];
+        k++, i++;
+    }
+    while (j <= n)
+    {
+        final[k] = c[j];
+        k++, j++;
+    }
+    for (int i = 1; i <= 2 * n; i++)
+    {
+        init[i] = final[i];
+    }
+}
 int main()
 {
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        scanf("%d", &a[i].val);
-        a[i].num = i;
-    }
-    stable_sort(a + 1, a + n + 1, cmp);
-    for (int i = 1; i <= n; i++)
-    {
-        add(a[i].num, 1);
-        ans += query(a[i].num - 1);
-    }
-    cout << ans << endl;
+    n = read(), R = read(), Q = read();
+    for (int i = 1; i <= 2 * n; i++)
+        init[i].n = i, init[i].score = read();
+    for (int i = 1; i <= 2 * n; i++)
+        init[i].k = read();
+    sort(init + 1, init + 1 + 2 * n, cmp);
+    for (int i = 0; i < R; i++)
+        work();
+    printf("%d", init[Q].n);
     return 0;
 }
